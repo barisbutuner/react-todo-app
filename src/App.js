d4c0d472df95873b9d298c9a24511a43b06793d9
Barış Bütüner <35434521+barisbutuner@users.noreply.github.com>
2023-01-15 17:53:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [newItem, setNewItem] = useState("");
+  const [items,setItems] = useState ([])
+
+
+
+  function addItem() {
+
+    if(!newItem){
+      alert("bir öğe girin");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random()*10000),
+      value: newItem
+    }
+    setItems(oldItems => [...oldItems,item])
+    setNewItem("");
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List App</h1>
+
+      <input 
+      type="text"
+      placeholder="Lütfen bir öğe ekle.."
+      value={newItem}
+      onChange={e => setNewItem(e.target.value)}
+      />
+
+      <button className="addButton" onClick={() => addItem()}>Ekle</button>
+      <ul>
+          {
+            items.map(item => {
+              return(
+                <li key={item.id}>{item.value} <button className='deleteButton' onClick={() => deleteItem(item.id)}>X</button> </li>
+              )
+            })
+          }
+      </ul>
     </div>
   );
 }
